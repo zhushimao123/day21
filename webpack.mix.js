621e20d4af3cc4d16ffda84ce8840dcf68f147a5
zhushimao123 <15235942622@163.com>
2019-10-09 09:41:59 +0800

    first commit

---
+++ webpack.mix.js
@@ -0,0 +1,54 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+Mix.listen('configReady', (webpackConfig) => {
+    // Exclude 'svg' folder from font loader
+    let fontLoaderConfig = webpackConfig.module.rules.find(rule => String(rule.test) === String(/(\.(png|jpe?g|gif|webp)$|^((?!font).)*\.svg$)/));
+    fontLoaderConfig.exclude = /(resources\/backend\/icons)/;
+});
+
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/backend'),
+            // '^': path.resolve(__dirname, 'resources/frontend'),
+            // 'common': path.resolve(__dirname, 'resources/common'),
+            // 'lang': path.resolve(__dirname, 'resources/common/lang'),
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.svg$/,
+                loader: 'svg-sprite-loader',
+                include: [path.resolve(__dirname, 'resources/backend/icons/svg')],
+                options: {
+                    symbolId: 'icon-[name]'
+                }
+            }
+        ],
+    }
+}).babelConfig({
+    plugins: ['dynamic-import-node']
+});
+
+// mix.js('resources/frontend/app.js', 'public/js')
+//     .js('resources/backend/main.js', 'public/js')
+//     .extract(['vue', 'axios']);
+//定义单一入口文件
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/backend/main.js', 'public/js')
+if (mix.inProduction()) {
+    mix.version();
+}
